c45cf140ba56cc1477ae0978b792b3dea1313c74
Cybertron-1 <119878133+Cybertron-1@users.noreply.github.com>
2023-07-23 22:46:37 +0000

    Update get_size.c

--- get_size.c
+++ get_size.c
@@ -2,7 +2,6 @@
 
 /**
  * get_size - Calculates the size to cast the argument
- * by Peter McMillan and Leonard Mbasela
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  *
